165af933d214dbf024d3018b6e61649c89dfce7e
yagmurkucukarslan <kucukarslanyagmur@gmail.com>
2025-09-10 16:59:55 +0300

    optionValueAdd function created

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const birimler = {
     sicaklik: ["Celsius", "Fahrenheit", "Kelvin"]
 }
 
+const selectName = ["selectWeight", "selectLength"];
+
 document.getElementById("conversionType").addEventListener("change",selectTransaction);
 
 function selectTransaction(){
@@ -15,20 +17,32 @@ function selectTransaction(){
     
     
     if(userSelection.value == "agirlik"){
-        denemeparagraf.innerHTML = "şuan agirliktasın"
-        for(let birim of birimlerSecilen){
-            console.log(birim)
-            document.getElementById("selectAgirlik").innerHTML += `<option value = "${birim}">${birim}</option>`;
-        }
+        denemeparagraf.innerHTML = "şuan agirliktasın";
+        optionValueAdd(birimlerSecilen)
     }
     else if(userSelection.value == "uzaklik"){
         denemeparagraf.innerHTML = "şuan uzakliktasın"
-
+        optionValueAdd(birimlerSecilen);
     }
     else if(userSelection.value == "sicaklik"){
         denemeparagraf.innerHTML = "şuan sicaklıktasın"
+        optionValueAdd(birimlerSecilen);
     }
     else{
         denemeparagraf.innerHTML = "bir şey seçmediniz"
+    }
+}
+
+function optionValueAdd(birimlerSecilen){
+    console.log(document.querySelectorAll(".selector"))
+    const selectors = document.querySelectorAll(".selector")
+    selectors[0].innerHTML = ""
+    selectors[1].innerHTML = ""
+    for(let birim of birimlerSecilen){
+        console.log(birim)
+        selectors.forEach(element => {
+            console.log(element)
+            element.innerHTML += `<option value = "${birim}">${birim}</option>`; 
+        });
     }
 }
